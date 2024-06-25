cbda6af24c14b5a7c0cb3a2dfef183d93c1b6183
Rahul Singh <rahulsingh.dev.36@gmail.com>
2024-06-25 22:43:42 +0530

    add update feature

--- User-dataBase/app.js
+++ User-dataBase/app.js
@@ -32,4 +32,15 @@ app.get('/delete/:id', async (req, res) => {
     res.redirect("/read");
 })
 
+app.get('/edit/:userid', async (req, res) => {
+    let user = await userModel.findOne({_id: req.params.userid});
+    res.render('edit', {user: user});
+});
+
+app.post('/update/:id', async (req, res) => {
+    let { name, email, image } = req.body; 
+    let user = await userModel.findOneAndUpdate({_id: req.params.id}, req.body, {new: true});
+    res.redirect("/read");
+});
+
 app.listen(3000);
